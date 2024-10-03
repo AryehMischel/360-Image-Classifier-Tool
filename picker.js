0b3dd99d82eb9f26ed5b07361c4edb1f3970e940
Aryeh Mischel <aryehmischel@gmail.com>
2024-10-03 13:22:35 -0400

    cleaning up unused code
    remove imageHandling.js as functionality as been moved to worker. removed dead and unused code

--- picker.js
+++ picker.js
@@ -217,11 +217,11 @@ async function checkFolder(folder) { //pass in folder name
             console.log(n5)
 
             if (cubemapFileNames.has(n) && cubemapFileNames.has(n1) && cubemapFileNames.has(n2) && cubemapFileNames.has(n3) && cubemapFileNames.has(n4) && cubemapFileNames.has(n5)) {
-                addImageUI(folder); 
-                savedImages.add(folder)
-                setUI(folder, "cubeMap")
+                // addImageUI(folder); 
+                // savedImages.add(folder)
+                // setUI(folder, "cubeMap")
 
-                makeCubeMap(parent, [files[0].id, files[1].id, files[2].id, files[3].id, files[4].id, files[5].id], [n, n1, n2, n3, n4, n5], folder)
+                // makeCubeMap(parent, [files[0].id, files[1].id, files[2].id, files[3].id, files[4].id, files[5].id], [n, n1, n2, n3, n4, n5], folder)
 
             } else {
                 // imagesLoading += files.length
